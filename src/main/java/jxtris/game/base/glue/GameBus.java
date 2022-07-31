a3ed6a57061311d718bb4d7daf608eb29ebaa611
4lineclear <tamimhakimi21@gmail.com>
2022-07-30 23:04:46 -0400

    More game work~ Designed piece placing system, is still messy, but works. Will next work on DAS and ARR.

--- src/main/java/jxtris/game/base/glue/GameBus.java
+++ src/main/java/jxtris/game/base/glue/GameBus.java
@@ -5,30 +5,51 @@ import jxtris.game.base.state.GameState;
 import jxtris.game.controls.Control;
 
 public class GameBus {
-    long drop, place, leftDas, rightDas;
-    boolean leftRotationLock, rightRotationLock, _180RotationLock, leftMoveLock, rightMoveLock;
+    private final long leftDAS, rightDAS, ARR;
+    private long dropTime, placeTime, placeCancelCounter, leftMoveTimer, rightMoveTimer, ARRTimer;
+    private boolean leftRotationLock, rightRotationLock, _180RotationLock ,leftMoveLock, rightMoveLock, placeCancelScheduled;
     private final BaseGame game;
     public GameBus(BaseGame game) {
         this.game = game;
+        leftDAS = 100;
+        rightDAS = 100;
+        ARR = 10;
     }
     public void increment(long elapsed){
-        drop += elapsed;
-        if (drop > 1000) {
-            drop = 0;
+        dropTime += elapsed;
+        if (dropTime > 1000) {
+            dropTime = 0;
             game.move(0,1);
         }
         if(!game.move(0, 1)) {
-            place+=elapsed;
-            if (place > 1000){
-                place = 0;
-                game.placeMino();
+            placeTime += elapsed;
+            if (placeCancelScheduled) {
+                placeTime -= elapsed;
+                placeTime += 10;
             }
-        }
-        else
-            game.move(0,-1);
+            if (placeTime > 500){
+                if(!placeCancelScheduled)
+                    placePiece();
+
+                else {
+                    placeCancelCounter++;
+                    placeTime = placeCancelCounter*50;
+                    placeCancelScheduled = false;
+                }
+            }
+
+        } else game.move(0, -1);
+
     }
     public GameState getState(){
         return game.getState();
+    }
+
+    private void placePiece(){
+        placeTime = 0;
+        placeCancelCounter = 0;
+        placeCancelScheduled = false;
+        game.placeMino();
     }
 
     public void throwKey(Control control) {
@@ -37,20 +58,32 @@ public class GameBus {
 
         switch (control){
             case MOVE_LEFT -> {
+                if(leftMoveLock) {
+                    return;
+                }
                 game.move(-1,0);
+                placeCancelScheduled = true;
+                leftMoveLock = true;
             }
             case MOVE_RIGHT -> {
+                if(rightMoveLock) {
+                    return;
+                }
                 game.move(1,0);
+                placeCancelScheduled = true;
+                rightMoveLock = true;
             }
             case ROTATE_LEFT -> {
                 if (leftRotationLock) return;
                 game.rotate(1);
                 leftRotationLock = true;
+                placeCancelScheduled = true;
             }
             case ROTATE_RIGHT -> {
                 if (rightRotationLock) return;
                 game.rotate(-1);
                 rightRotationLock = true;
+                placeCancelScheduled = true;
             }
             case SOFT_DROP -> {
                 game.move(0,1);
@@ -59,15 +92,17 @@ public class GameBus {
                 while (true)
                     if (!game.move(0, 1)) break;
 
-                game.placeMino();
+                placePiece();
             }
             case ROTATE_180 -> {
                 if(_180RotationLock) return;
                 game.rotate(2);
                 _180RotationLock = true;
+                placeCancelScheduled = true;
             }
             case HOLD -> {
                 game.hold();
+                placeCancelScheduled = true;
             }
             case RESTART -> {
             }
@@ -81,8 +116,10 @@ public class GameBus {
             return;
         switch (control){
             case MOVE_LEFT -> {
+                leftMoveLock = false;
             }
             case MOVE_RIGHT -> {
+                rightMoveLock = false;
             }
             case ROTATE_LEFT ->{
                 leftRotationLock = false;
